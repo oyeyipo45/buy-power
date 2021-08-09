71f3f04ab2157815d1a998a2e9f8b4d1a668bc60
damilola <oyeyipo45@gmail.com>
2021-08-09 11:04:41 +0100

    add notification to process auctions

--- auction-service/src/lib/closeAuction.js
+++ auction-service/src/lib/closeAuction.js
@@ -7,6 +7,10 @@ const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 
 export async function closeAuction(auction) {
+
+
+    const sqs = new AWS.SQS()
+
     const params = {
         TableName: process.env.AUCTIONS_TABLE_NAME,
         Key: { id: auction.id },
@@ -18,8 +22,33 @@ export async function closeAuction(auction) {
             '#status' : 'status'
         }
     }
+ 
+
+    await dynamodb.update(params).promise()
+
+    const { title, seller, highestBid } = auction
+    const { amount, bidder } = highestBid
+
+    const notifySeller = sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: 'Your item has been sold',
+            recipitent: seller,
+            body: `Your item "${title}" has been sold for $${amount}`
+        })
+    }).promise()
 
 
-    const result = await dynamodb.update(params).promise()
-    return result;
+    const notifyBidder = sqs
+      .sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+          subject: 'You won an auction',
+          recipitent: bidder,
+          body: `You just scored yourself a great deal buying "${title}" for the amount of $${amount}`,
+        }),
+      })
+        .promise();
+    
+    return Promise.all([notifyBidder, notifySeller])
 }
