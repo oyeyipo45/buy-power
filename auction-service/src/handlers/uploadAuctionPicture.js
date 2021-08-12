d1acf0d2b378ff775eae2d3c441aaa0558281210
damilola <oyeyipo45@gmail.com>
2021-08-12 09:16:53 +0100

    update seller no bid was made

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,27 @@
+import { getAuctionById } from './getAuction'
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import createError from 'http-errors';
+
 export async function uploadAuctionPicture(event) {
+
+    const { id } = event.pathParameters
+
+    const auction = await getAuctionById(id)
+
+    if (!auction) {
+      throw new createError.NotFound(`Auction with ID "${id}" not found`);
+    }
+
+    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64, 'base64');
+
+    const uploadPictureResult = await uploadPictureToS3(auction.id + '.jpg', buffer)
+
+    // const uploadPictureResult = await uploadPictureToS3(`${auction.id}.jpg, ${buffer}`);
+
+    console.log(uploadPictureResult);
+
+
     
     return {
         statusCode: 200,
